d6339010a925f845a0707e3a9ea5491e9990bf31
Nyx-Off <contact@bensalemdev.fr>
2024-06-05 15:14:16 +0200

    exo1 finit

--- PF/Exo1/pourboire.js
+++ PF/Exo1/pourboire.js
@@ -1,23 +1,27 @@
-function calculPourboire(montant, service) {
-    var pourcentagePourboire;
-
-    service = service.toLowerCase();
-
-    if (service === "terrible") {
-        pourcentagePourboire = 0;
-    } else if (service === "poor") {
-        pourcentagePourboire = 0.05;
-    } else if (service === "good") {
-        pourcentagePourboire = 0.1;
-    } else if (service === "great") {
-        pourcentagePourboire = 0.15;
-    } else if (service === "excellent") {
-        pourcentagePourboire = 0.2;
-    } else {
-        return "Rating not recognised";
+function calculateTip(amount, rating) {
+    let tipPercentage;
+    switch (rating.toLowerCase()) {
+        case 'terrible':
+            tipPercentage = 0;
+            break;
+        case 'poor':
+            tipPercentage = 0.05;
+            break;
+        case 'good':
+            tipPercentage = 0.1;
+            break;
+        case 'great':
+            tipPercentage = 0.15;
+            break;
+        case 'excellent':
+            tipPercentage = 0.2;
+            break;
+        default:
+            return "Rating not recognised";
     }
-
-    var pourboire = Math.ceil(montant * pourcentagePourboire);
-
-    return pourboire;
+    return Math.ceil(amount * tipPercentage);
 }
+
+console.log(calculateTip(20, "ExcellEnt"));  
+console.log(calculateTip(26.95, "good")); 
+console.log(calculateTip(20, "hi")); 
