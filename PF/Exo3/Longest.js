7ac9fc7dc907f56165400d97ee9023208c7b1360
Nyx-Off <contact@bensalemdev.fr>
2024-06-05 15:35:13 +0200

    exo3 finit

---
+++ PF/Exo3/Longest.js
@@ -0,0 +1,9 @@
+function longestWord(str) {
+    return str.split(' ').reduce((longest, currentWord) => {
+        return currentWord.length >= longest.length ? currentWord : longest;
+    }, '');
+}
+
+console.log(longestWord('a b c d e fgh')); 
+console.log(longestWord('one two three')); 
+console.log(longestWord('red blue grey')); 
