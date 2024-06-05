f2b1ae999165de06bffdf9adeb440faf0bc2d4b9
Nyx-Off <contact@bensalemdev.fr>
2024-06-05 12:39:13 +0200

    modification main pour test

--- POO/Main.java
+++ POO/Main.java
@@ -3,17 +3,29 @@ import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-        Etudiant etudiant = new Etudiant("Dupont", "Jean", 123, new Date(), "Master");
+    Etudiant etudiant = new Etudiant("Dupont", "Jean", 123, new Date(), "Master");
 
-        System.out.println("Nom: " + etudiant.getNom());
-        System.out.println("Prénom: " + etudiant.getPrenom());
-        System.out.println("Identifiant: " + etudiant.getIdentifiant());
-        System.out.println("Date d'inscription: " + etudiant.getDateInscription());
-        System.out.println("Niveau d'étude: " + etudiant.getNiveauEtude());
+    etudiant.montrerDetails();
+    System.out.println("Date d'inscription: " + etudiant.getDateInscription());
+    System.out.println("Niveau d'étude: " + etudiant.getNiveauEtude());
 
-        etudiant.setNiveauEtude("Doctorat");
-        System.out.println("Nouveau niveau d'étude: " + etudiant.getNiveauEtude());
+    etudiant.setNiveauEtude("Doctorat");
+    System.out.println("Nouveau niveau d'étude: " + etudiant.getNiveauEtude());
 
+    Cours cours = new Cours();
+    cours.setTitre("Programmation en Java");
+    cours.setFormateur("M. Dupont");
+    cours.setDuree(2);
+    cours.setPlacesRestantes(30);
+
+    cours.montrerDetails();
+
+    cours.participer();
+
+    System.out.println("Places restantes après la participation de l'étudiant: " + cours.getPlacesRestantes());
+
+    cours.quitter();
+
+    System.out.println("Places restantes après que l'étudiant ait quitté le cours: " + cours.getPlacesRestantes());
     }
-
 }
